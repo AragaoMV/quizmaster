9834882bbec595ef6f33f29e866e3fe086c67421
AragaoMV <marcosvinisa@icloud.com>
2025-02-12 16:10:04 -0300

    Implemented a timer system to manage question answering and feedback phases. The UpdateTimer function displays the remaining time to answer the question and the time to review the correct answer before moving to the next question. The timer sprite must be added to the game to visually represent the countdown.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    public bool loadNextQuestion;
+    public bool isAnsweringQuestion = false
+    public float fillFraction;
+    float timerValue;
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+    void UpdateTimer()
+    {
+        timerValue -= Timer.deltaTime;
+
+        if (isAnsweringQuestion) //Current Time to answer the question
+        {
+            if (timeValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+
+            }
+            else
+            {
+                isAnsweringQuestion = false
+                timerValue = timeToShowCorrectAnswer;
+            }
+
+        }
+        else //Current Time to show the correct answer
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+
+            }
+            else
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true
+            }
+        }
+    }
+}
